86777512a7a1af6cd26af65761da83ee784be437
Eric Fleming <efleming111@yahoo.com>
2018-05-17 14:27:51 -0700

    Started Physics

--- 2DEngine_Win32/2DEngine_Win32/src/engine/physics/EPhysics.cpp
+++ 2DEngine_Win32/2DEngine_Win32/src/engine/physics/EPhysics.cpp
@@ -5,5 +5,84 @@
 //  5/10/2018
 //
 
+#include "EPhysics.h"
+#include "../utilities/ETimer.h"
 
+void PhysicsBody::Create(EPhysics* physics, PhysicsBodyType type, float x, float y, std::string name)
+{
+	m_Name = name;
+
+	b2BodyDef bodyDef;
+	bodyDef.type = (b2BodyType)type;
+	bodyDef.position.Set(x, y);
+	bodyDef.fixedRotation = true;
+	bodyDef.userData = (void*)this;
+
+	m_Body = physics->GetWorld()->CreateBody(&bodyDef);
+}
+
+void PhysicsBody::AddBoxCollider(float x, float y, float width, float height, bool isSensor)
+{
+	b2PolygonShape box;
+	box.SetAsBox(width * 0.5f, height * 0.5f, b2Vec2(x, y), 0.0f);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &box;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+	fixtureDef.isSensor = isSensor;
+
+	m_Body->CreateFixture(&fixtureDef);
+}
+
+void ContactListener::BeginContact(b2Contact* contact)
+{
+	PhysicsBody* BodyA = static_cast<PhysicsBody*>(contact->GetFixtureA()->GetBody()->GetUserData());
+	PhysicsBody* BodyB = static_cast<PhysicsBody*>(contact->GetFixtureB()->GetBody()->GetUserData());
+
+	BodyA->BeginContact(BodyB);
+	BodyB->BeginContact(BodyA);
+}
+
+void ContactListener::EndContact(b2Contact* contact)
+{
+	PhysicsBody* BodyA = static_cast<PhysicsBody*>(contact->GetFixtureA()->GetBody()->GetUserData());
+	PhysicsBody* BodyB = static_cast<PhysicsBody*>(contact->GetFixtureB()->GetBody()->GetUserData());
+
+	BodyA->EndContact(BodyB);
+	BodyB->EndContact(BodyA);
+}
+
+bool EPhysics::Initialize(float xGravity, float yGravity)
+{
+	b2Vec2 gravity(xGravity, yGravity);
+	m_World = new b2World(gravity);
+
+	m_AccumTime = 0.0f;
+
+	m_VelocityIterations = 6;
+	m_PositionIterations = 2;
+
+	m_World->SetContactListener(&m_ContactListener);
+
+	return true;
+}
+
+void EPhysics::Shutdown()
+{
+	// TODO: Check if anything else needs deleted
+
+	delete m_World;
+}
+
+void EPhysics::Update()
+{
+	m_AccumTime += lilTimer->DeltaTime();
+	if (m_AccumTime < PHYSICS_TIME_STEP)
+		return;
+
+	m_AccumTime -= PHYSICS_TIME_STEP;
+
+	m_World->Step(PHYSICS_TIME_STEP, m_VelocityIterations, m_PositionIterations);
+}
 
--- 2DEngine_Win32/2DEngine_Win32/src/engine/physics/EPhysics.h
+++ 2DEngine_Win32/2DEngine_Win32/src/engine/physics/EPhysics.h
@@ -7,5 +7,80 @@
 
 #pragma once
 
+#include <string>
 
+#include <Box2D\Box2D.h>
+
+#define EVector2D b2Vec2
+#define EMax b2Max
+#define EMin b2Min
+
+enum PhysicsBodyType {
+	STATIC_BODY = b2_staticBody,
+	KINEMATIC_BODY = b2_kinematicBody,
+	DYNAMIC_BODY = b2_dynamicBody
+};
+
+const float PHYSICS_TIME_STEP = 1.0f / 60.0f;
+
+class PhysicsBody;
+
+class ContactListener : public b2ContactListener
+{
+	void BeginContact(b2Contact* contact);
+	void EndContact(b2Contact* contact);
+};
+
+class EPhysics
+{
+public:
+	EPhysics() {}
+	~EPhysics() {}
+
+	bool Initialize(float xGravity, float yGravity);
+	void Shutdown();
+	void Update();
+
+	b2World* GetWorld() { return m_World; }
+
+private:
+	b2World * m_World;
+
+	int32 m_VelocityIterations;
+	int32 m_PositionIterations;
+
+	ContactListener m_ContactListener;
+
+	float m_AccumTime;
+
+private:
+	EPhysics(const EPhysics& physics) {}
+	void operator=(const EPhysics& physics) {}
+};
+
+class PhysicsBody
+{
+public:
+	PhysicsBody() {}
+	virtual ~PhysicsBody() {}
+
+	void Create(EPhysics* physics, PhysicsBodyType type, float x, float y, std::string name);
+
+	void AddBoxCollider(float x, float y, float width, float height, bool isSensor);
+
+	virtual void BeginContact(PhysicsBody* other) {}
+	virtual void EndContact(PhysicsBody* other) {}
+
+	std::string& GetName() { return m_Name; }
+
+	EVector2D GetVelocity() { return m_Body->GetLinearVelocity(); }
+	EVector2D GetPosition() { return m_Body->GetPosition(); }
+
+	void SetVelocity(EVector2D vector) { m_Body->SetLinearVelocity(vector); }
+
+private:
+	b2Body * m_Body;
+
+	std::string m_Name;
+};
 
